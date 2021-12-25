ea96d05c493dba594a6f28482d8fda18a69124a7
Quentin Chin <q.chin@hotmail.com>
2021-12-24 23:26:34 -0500

    Edit styling for products page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,17 +3,19 @@ import "./Product.css";
 
 const Product = ({ product }) => {
   return (
-    <li key={product.id} className="product">
-      <div className="product-img">
-        <div className="product-img-container">
-          <img src={product.media.source} alt="product" />
+    <div className="product-wrapper">
+      <li key={product.id} className="product">
+        <div className="product-img">
+          <div className="product-img-container">
+            <img src={product.media.source} alt="product" />
+          </div>
         </div>
-      </div>
-      <div className="product-desc">
-        <h3 className="product-title">{product.name}</h3>
-        <p className="product-price">{product.price.formatted_with_symbol}</p>
-      </div>
-    </li>
+        <div className="product-desc">
+          <h3 className="product-title">{product.name}</h3>
+          <p className="product-price">{product.price.formatted_with_symbol}</p>
+        </div>
+      </li>
+    </div>
   );
 };
 
