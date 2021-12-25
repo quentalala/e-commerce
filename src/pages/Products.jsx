c4d5532b960ce8013e22107c1333422ea916764a
Quentin Chin <q.chin@hotmail.com>
2021-12-24 23:00:18 -0500

    Add grid from material ui to display products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,26 +2,45 @@ import React from "react";
 import Product from "../components/Product";
 import { Link } from "react-router-dom";
 import "./Products.css";
+import { Grid } from "@mui/material";
 
 const Products = ({ products }) => {
   return (
     <>
       <h1 className="page-title">Products</h1>
       <div className="products-container">
-        <ul className="products">
+        <Grid
+          container
+          spacing={5}
+          justifyContent="center"
+          className="products"
+        >
           {products.map((product) => {
             return (
-              <Link
-                to={`/products/${product.id}`}
-                className="product-container"
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                xl={3}
                 key={product.id}
-                style={{ textDecoration: "none" }}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  outline: "1px",
+                }}
               >
-                <Product product={product} />
-              </Link>
+                <Link
+                  to={`/products/${product.id}`}
+                  className="product-container"
+                  style={{ textDecoration: "none" }}
+                >
+                  <Product product={product} />
+                </Link>
+              </Grid>
             );
           })}
-        </ul>
+        </Grid>
       </div>
     </>
   );
